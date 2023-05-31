eea52b42b9bd848076ddef77389c873741d60744
DevGaeRobby <130748559+DevGaeRobby@users.noreply.github.com>
2023-05-31 23:56:46 +0900

    Add files via upload

--- 1.js
+++ 1.js
@@ -13,8 +13,10 @@ $btns.forEach($btn => {
 //Btns는 배열이므로 forEach로 나눈다.
 //여기서target은 btn(버튼)이다.
 //target을 쓰는 이유는 클로저 때문,
-//target대신 btn을 적으면 생성 환경을 저장하는 클로저 때문에 메모리를 엄청 먹는다.
+//target대신 btn을 적으면 생성 환경을 저장하는 클로저 때문에
+//btn을 적은 코드가 많을수록 메모리를 엄청 먹는다.
 
 $result.addEventListener('click', function(event){
     alert(result.join(' '));
 })
+//click
---
+++ 6.js
@@ -0,0 +1,22 @@
+const array = [['1-1', '1-2'], ['2-1', '2-2'], ['3-1', '3-2']]
+
+for (i = 0; i < array.length; i++) {
+    for (j = 0; j < array[i].length; j++)
+    console.log(array[i][j]);
+}
+
+console.log('---------')
+
+for (const value of array) {
+        for (const no of value){
+            console.log(no);
+        }
+};
+
+console.log('---------')
+
+array.forEach(function(arr){
+    arr.forEach(function(brr){
+        console.log(brr);
+    })
+})
